ccb441fe0f3cce68ecd9117d2a7507dfe1a950f9
bolsakova <bolsakova@github.com>
2026-06-20 12:01:02 +0200

    ex02: created directory with necessary files, Makefile copied from ex01

---
+++ ex02/inc/MutantStack.hpp
@@ -0,0 +1,17 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <exception>
+#include <vector>
+
+/**
+ * @brief 
+ */
+class MutantStack {
+	private:
+	public:
+};
+
+#include "MutantStack.tpp"
+
+#endif
---
+++ ex02/src/main.cpp
@@ -0,0 +1,13 @@
+#include "../inc/MutantStack.hpp"
+#include <iostream>
+#include <vector>
+#include <random>
+
+static void printHeader(const char* title) {
+	std::cout << "\n=== " << title << " ===\n";
+}
+
+int main() {
+	std::cout << "\n=== All tests completed! ===\n";
+	return 0;
+}
