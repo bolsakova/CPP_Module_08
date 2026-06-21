29722fefaea6175b326c4df88a5381f9a39e1ecc
bolsakova <bolsakova@github.com>
2026-06-21 21:49:02 +0200

    ex02: MutantStack.hpp - created template class that exposes underlying containers iterator

--- ex02/inc/MutantStack.hpp
+++ ex02/inc/MutantStack.hpp
@@ -1,15 +1,37 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
-#include <exception>
-#include <vector>
+#include <stack>
 
 /**
- * @brief 
+ * @brief MutantStack
+ * - thin wrapper around std::stack that exposes the underlying container's iterators
+ * - template class; uses std::stack<T>::container_type for iterator types.
  */
-class MutantStack {
-	private:
+template <typename T>
+class MutantStack : public std::stack<T> {
 	public:
+		typedef typename std::stack<T>::container_type container_type;
+
+		typedef typename container_type::iterator iterator;
+		typedef typename container_type::const_iterator const_iterator;
+		typedef typename container_type::reverse_iterator reverse_iterator;
+		typedef typename container_type::const_reverse_iterator const_reverse_iterator;
+
+		MutantStack() = default;
+		MutantStack(const MutantStack& other) = default;
+		MutantStack& operator=(const MutantStack& other) = default;
+		~MutantStack() = default;
+
+		iterator begin();
+		iterator end();
+		const_iterator begin() const;
+		const_iterator end() const;
+
+		reverse_iterator rbegin();
+		reverse_iterator rend();
+		const_reverse_iterator rbegin() const;
+		const_reverse_iterator rend() const;
 };
 
 #include "MutantStack.tpp"
